049fd4af01fd3d4b0661f8529d5ba6e0b147a227
sismi joseph <sismi@reidis.io>
2025-10-16 19:08:25 +0530

    completed blog app

---
+++ blog/blogapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+def home(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'home.html', {'posts': posts})
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id)
+    return render(request, 'post_detail.html', {'post': post})
